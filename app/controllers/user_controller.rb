d9a1e6248ecbd8231f4db97074322db279e4213f
Dev <new@temp.com>
2019-09-03 18:09:32 +0530

    Modification Done in Profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
 
   end
   
-  def profile
+  def show
     @user = User.find(params[:id]) 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user do
-    member do 
-      get 'profile'
-    end 
-  end
+  resources :user 
 
   resources :products
   
