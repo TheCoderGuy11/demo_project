985636fa2666724fa79107805cf1d34b0003ced3
Dev <new@temp.com>
2019-09-05 16:18:32 +0530

    Installation of Active Admin + CanCanCan authorization require heading

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  load_and_authorize_resource
+  
   def index
     @orders = current_user.orders.paginate(page: params[:page], per_page: 6) 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_080901) do
+ActiveRecord::Schema.define(version: 2019_09_05_103234) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
