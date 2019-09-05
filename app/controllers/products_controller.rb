96249c63873427acc3cf5705c05057847746fc2d
Dev <new@temp.com>
2019-09-05 19:52:25 +0530

    Handling data flow with intermediate table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,7 @@ class ProductsController < ApplicationController
   def create
     @product = current_user.products.new(product_params)
     if @product.save
+      create_category_product(@product)
       HardWorker.perform_at(1.minutes.from_now, @product.name)
       redirect_to products_path
     else
@@ -47,8 +48,12 @@ class ProductsController < ApplicationController
   end 
 
   private
-    def product_params
-      params.require(:product).permit(:name, :price, :quantity, :brand_name, images_attributes: [:file, :_destroy], delivery_time_attributes: [:number_of_days])
-    end
 
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity, :brand_name, images_attributes: [:file, :_destroy], delivery_time_attributes: [:number_of_days])
+  end
+
+  def create_category_product(product)
+    product.create_category_product(category_id: params[:product]['category'])
+  end
 end
